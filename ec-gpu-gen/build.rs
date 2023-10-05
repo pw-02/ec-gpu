688253e43841667f0277e99d2eb39a39359e0fba
pw-02 <watters.patrick@outlook.com>
2023-10-04 21:41:01 -0700

    cuda

--- ec-gpu-gen/build.rs
+++ ec-gpu-gen/build.rs
@@ -34,7 +34,7 @@ fn main() {
         Ok(args) => execute::command(format!("nvcc {}", args)),
         Err(_) => {
             let mut command = Command::new("nvcc");
-           /* 
+           
             command
                 .arg("--optimize=6")
                 // Compile with as many threads as CPUs are available.
@@ -44,7 +44,7 @@ fn main() {
                 .arg("--generate-code=arch=compute_86,code=sm_86")
                 .arg("--generate-code=arch=compute_80,code=sm_80")
                 .arg("--generate-code=arch=compute_75,code=sm_75");
-            */
+            /* 
             command
                 .arg("--optimize=6")
                 //.arg("--threads=0")
@@ -53,6 +53,7 @@ fn main() {
                 .arg("--generate-code=arch=compute_61,code=sm_61")
                 .arg("--generate-code=arch=compute_60,code=sm_60")
                 .arg("--generate-code=arch=compute_52,code=sm_52");
+            */
             command
         }
     };
